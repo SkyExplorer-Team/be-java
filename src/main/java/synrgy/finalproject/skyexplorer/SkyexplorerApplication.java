e51cde9bd1e4a14a8cd373efbd4e10d50317ccfe
ariffjrp <batiarari290@gmail.com>
2024-01-04 23:00:40 +0700

    Initial commit

---
+++ src/main/java/synrgy/finalproject/skyexplorer/SkyexplorerApplication.java
@@ -0,0 +1,13 @@
+package synrgy.finalproject.skyexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyexplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyexplorerApplication.class, args);
+	}
+
+}
