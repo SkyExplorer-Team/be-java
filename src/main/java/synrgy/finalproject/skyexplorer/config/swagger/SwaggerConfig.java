7b8e3f2c0bd95d5c3ab4b3552dbea25eec1bee1f
Ersalomo SJS <95175703+ersalomo@users.noreply.github.com>
2024-02-05 22:14:10 +0700

    Update SwaggerConfig.java

--- src/main/java/synrgy/finalproject/skyexplorer/config/swagger/SwaggerConfig.java
+++ src/main/java/synrgy/finalproject/skyexplorer/config/swagger/SwaggerConfig.java
@@ -12,7 +12,7 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default server url bre")})
+@OpenAPIDefinition(servers = {@Server(url = "/api/", description = "Default server url bre")})
 @Configuration
 public class SwaggerConfig {
     @Bean
