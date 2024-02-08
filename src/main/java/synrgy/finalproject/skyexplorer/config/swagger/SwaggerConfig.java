c7fa9a0a1fe8a1de34a774c8df8202e0bd2bf57d
ersalomo <ersalomo2002@gmail.com>
2024-02-08 16:54:37 +0700

    fxed error cors

--- src/main/java/synrgy/finalproject/skyexplorer/config/swagger/SwaggerConfig.java
+++ src/main/java/synrgy/finalproject/skyexplorer/config/swagger/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package synrgy.finalproject.skyexplorer.config.swagger;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
@@ -11,6 +13,7 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(servers = {@Server(url = "/api/", description = "Default server url bre")})
 @Configuration
 public class SwaggerConfig {
     @Bean
@@ -23,11 +26,12 @@ public class SwaggerConfig {
                         .title("Sky Explorer REST API")
                         .description("Dokumentasi Rest API Skyexplorer everybody")
                         .version("1.0").contact(new Contact().name("Ersalomo and team skyexplorer")
-                                .email( "www.baeldung.com").url("ersalomo2002@gmail.com"))
-                        .license(new License().name("License of API").url("API license URL")));
+                                .email("ersalomo2002@gmail.com").url("https://example.com"))
+                        .license(new License().name("License of API").url("https://example.com")));
     }
     private SecurityScheme createAPIKeyScheme() {
-        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
                 .bearerFormat("JWT")
                 .scheme("bearer");
     }
--- src/main/java/synrgy/finalproject/skyexplorer/security/WebMvcConfig.java
+++ src/main/java/synrgy/finalproject/skyexplorer/security/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package synrgy.finalproject.skyexplorer.security;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.Arrays;
 
 @Configuration
+@Slf4j
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
@@ -17,6 +19,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        log.info("000000000000000000000000000000 {}", allowedOrigins.length);
+        log.info("urls {}", Arrays.stream(allowedOrigins).toList());
         registry.addMapping("/**")
                 .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
