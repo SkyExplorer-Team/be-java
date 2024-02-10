6ed337d4af2374339aca4aaf31260e8d58d562ce
ersalomo <ersalomo2002@gmail.com>
2024-02-10 19:45:26 +0700

    .

--- src/main/java/synrgy/finalproject/skyexplorer/security/WebMvcConfig.java
+++ src/main/java/synrgy/finalproject/skyexplorer/security/WebMvcConfig.java
@@ -18,8 +18,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//                .allowedOrigins(allowedOrigins)
-//                .allowedOrigins("*")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
