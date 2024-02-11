d1d5472623c5ba5f0e0c7c9c4280d4351f5c2856
ariffjrp <batiarari290@gmail.com>
2024-02-11 09:27:47 +0700

    casting date

--- src/main/java/synrgy/finalproject/skyexplorer/repository/ScheduleRepository.java
+++ src/main/java/synrgy/finalproject/skyexplorer/repository/ScheduleRepository.java
@@ -17,7 +17,7 @@ public interface ScheduleRepository extends JpaRepository<Schedule, UUID> {
     @Query("SELECT s FROM Schedule s " +
             "WHERE s.from.id = :fromId " +
             "AND s.to.id = :toId " +
-            "AND s.timeDeparture >= :startDate " +
+            "AND CAST(s.timeDeparture AS date) >= :startDate " +
             "ORDER BY s.timeDeparture ASC")
     List<Schedule> findSchedules(
             @Param("fromId") UUID fromId,
